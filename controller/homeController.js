f4cc63644eb8d79d08003fccd15099f11d3282cd
neelabhnagaich <neelabhnagaich@gmail.com>
2021-12-24 12:21:16 +0530

    bigPromise for errorhandling in controllers

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,12 +1,28 @@
-exports.home = (req,res)=>{
+const BigPromise = require('../middleware/bigPromise')
+
+exports.home = BigPromise(async(req,res)=>{
+
+    // const db = await somethingfromDB()
     res.status(200).json({
         success: true,
         "greeting":"Hello from API"
     })
-}
+})
+
+
+// alternative way of above used here as try catch
 exports.home = (req,res)=>{
-    res.status(200).json({
-        success: true,
-        "greeting":"this is a dummy route"
-    })
+    try {
+
+        // const db = await somethingfromDB()
+        res.status(200).json({
+            success: true,
+            "greeting":"this is a dummy route"
+        })
+
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
